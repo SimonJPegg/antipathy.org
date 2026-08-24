1a13dc6ee4dfce1fa750efeb70d31214e305cf31
Ciaran Kearney <ciaran@antipathy.org>
2026-08-24 20:32:52 +0100

    Remove Cloudflare adapter, use pure static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://www.antipathy.org",
   integrations: [mdx()],
-  adapter: cloudflare({ mode: "directory" }),
+  output: "static",
   image: {
     service: {
       entrypoint: "astro/assets/services/sharp",
